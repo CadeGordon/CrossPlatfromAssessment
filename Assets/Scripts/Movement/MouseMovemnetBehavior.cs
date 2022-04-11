e64395b113261e46cabbc2a3aa23cd0939c5a717
Cade Gordon <cadegordon093@gmail.com>
2022-04-11 15:42:47 -0500

    removed old mouse movement code

--- Assets/Scripts/Movement/MouseMovemnetBehavior.cs
+++ Assets/Scripts/Movement/MouseMovemnetBehavior.cs
@@ -5,15 +5,6 @@ using UnityEngine;
 public class MouseMovemnetBehavior : MonoBehaviour
 {
     private PlayerMovementBehavior _mouseMovement;
-
-    private Rigidbody _rigidbody;
-    [SerializeField]
-    private Camera _camera;
-    private Vector3 _lastMousePos;
-    private Vector3 _mousePos;
-    private Vector3 _newPos;
-    private Transform _transform;
-    private Transform _localTransform;
     [SerializeField]
     public float _speed;
 
@@ -21,40 +12,7 @@ public class MouseMovemnetBehavior : MonoBehaviour
     {
         _mouseMovement = GetComponent<PlayerMovementBehavior>();
 
-        //_transform = GetComponent<Transform>();
-        //_localTransform = GetComponent<Transform>();
     }
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (Input.GetMouseButton(0))
-    //    {
-    //        _mousePos = _camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
-
-    //        float xPos = _mousePos.x - _lastMousePos.x;
-
-    //        _newPos.x = _localTransform.position.x - xPos;
-    //        _newPos.y = _localTransform.position.y;
-    //        _newPos.z = 0;
-
-    //        _localTransform.position = _newPos + _localTransform.forward * _speed * Time.deltaTime;
-
-    //        _lastMousePos = _mousePos;
-    //    }
-    //}
-
-    //private void FixedUpdate()
-    //{
-    //    Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-    //    RaycastHit hitInfo;
-
-    //    //If the ray hits an object
-    //    if (Physics.Raycast(ray, out hitInfo))
-    //    {
-    //        //do mouse stuff
-    //    }
-    //}
 
     void Update()
     {
